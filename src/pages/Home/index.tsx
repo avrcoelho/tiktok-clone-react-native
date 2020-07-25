6e9269c80ee026fa1afa354a31e3e001bc21ab71
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-07-25 15:30:50 -0300

    feat: routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
